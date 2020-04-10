093cd416921065ba914c43e825ecd42042846726
goohooh <piriri11@gmail.com>
2020-04-11 08:20:42 +0900

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -20,32 +20,4 @@ export default props => {
             <OffCanvas isMenuOpened={isMenuOpened} setIsMenuOpened={setIsMenuOpened} />
         </>
     );
-    // return (
-    //     <>
-    //         <Head>
-    //             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    //         </Head>
-    //         <OffCanvas
-    //             width={300}
-    //             transitionDuration={300}
-    //             effect={"parallax"}
-    //             isMenuOpened={isMenuOpened}
-    //             position={"right"}
-    //         >
-    //             <OffCanvasBody className="main">
-    //                 <Header {...props} setIsMenuOpened={setIsMenuOpened} />
-    //                 {props.children}
-    //             </OffCanvasBody>
-    //             <OffCanvasMenu className="off-canvas shadow">
-    //                 <div className="p-4">
-    //                     <div className="flex justify-between">
-    //                         <h3>My</h3>
-    //                         <button onClick={() => setIsMenuOpened(false)}>X</button>
-    //                     </div>
-    //                     <LoginForm />
-    //                 </div>
-    //             </OffCanvasMenu>
-    //         </OffCanvas>
-    //     </>
-    // );
 };
--- components/OffCanvas.js
+++ components/OffCanvas.js
@@ -2,11 +2,14 @@ import LoginForm from './LoginForm';
 
 export default ({ isMenuOpened, setIsMenuOpened }) => (
     <div className="fixed top-0 w-screen h-screen box-border flex flex-row-reverse" style={{
-        right: isMenuOpened ? "0%" : "-100%",
+        left: isMenuOpened ? "0%" : "100%",
         zIndex: 10000,
         backgroundColor: "rgba(0 ,0 ,0, 0.6)"
     }}>
-        <div className="w-4/5 h-screen bg-white p-4 box-border">
+        <div className="fixed top-0 w-4/5 h-screen bg-white p-4 box-border transition duration-100" style={{
+            right: isMenuOpened ? "0%" : "-100%",
+            zIndex: 10001,
+        }}>
             <div className="flex justify-between">
                 <h3>My</h3>
                 <button onClick={() => setIsMenuOpened(false)}>X</button>
