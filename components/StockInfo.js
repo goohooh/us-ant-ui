6a53a8545ceecc54d75ccaed997adf3d5e33183b
goohooh <goo_oo@naver.com>
2020-04-25 10:48:13 +0900

    header style

--- components/StockInfo.js
+++ components/StockInfo.js
@@ -4,10 +4,10 @@ const StockInfo = ({ stock, onClick }) => {
     return (
         <div className="header__info items-center" onClick={onClick}>
             <div>
-                <div className="header__info--name text-xs">{companyName}</div>
-                <div className="header__info--price text-xs">{latestPrice}$</div>
+                <div className="header__info--name">{companyName}</div>
+                <div className="header__info--price">{latestPrice}$</div>
             </div>
-            <div className="ml-2 flex aligin-center">
+            <div className="ml-2 flex aligin-center header__info--search">
                 <span className="material-icons">search</span>
             </div>
         </div>
