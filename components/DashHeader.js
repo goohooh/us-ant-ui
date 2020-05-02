071f4241ed1c4bb181760d28c53f7c7c87853744
goohooh <piriri11@gmail.com>
2020-05-02 14:11:19 +0900

    init my

---
+++ components/DashHeader.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+const headerStyle = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  padding: '14px 20px',
+  width: '100%',
+  height: '70',
+  background: '#e0e5ec',
+  zIndex: 1000
+};
+
+const linkStyle = {
+  width: 60,
+  flexBasis: 60,
+  flexShrink: 0,
+  marginRight: 15
+};
+
+const DashboardHeader = () => {
+    return (
+      <div style={headerStyle} className="flex flex-row justify-between items-center shadow-sm">
+        <Link href="/">
+          <a style={linkStyle}>
+            <img src="/logo.png" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default DashboardHeader;
